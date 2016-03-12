419782acbbeb5351345129aa2ef377c9456105f4
rozklad <jan.rozklad@gmail.com>
2016-03-12 21:08:31 +0100

    defaults

---
+++ types.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	'option' => [
+		'file' => 'File',
+		'multifile' => 'Multifile',
+		'avatar' => 'Avatar',
+		'switchery' => 'Switchery',
+		'dropzone' => 'Dropzone',
+		'truefalse' => 'True/False',
+		'repeater' => 'Repeater',
+		'wysiwyg' => 'WYSIWYG',
+	],
+
+	'file' => [
+		'select' => 'Vybrat soubory',
+		'allowed' => 'Povolené typy souborů',
+		'click_upload' => 'Kliknutím nahrajte',
+	],
+
+];
